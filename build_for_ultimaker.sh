a647a3fc378079e1c504806a6dd5b0aa278c1664
Tiemen Joustra <t.joustra@ultimaker.com>
2019-12-06 16:54:11 +0100

    Added fix for git submodules when switching between master and master-next_som branches

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -47,7 +47,12 @@ env_check()
 
 run_build()
 {
-    git submodule update --init --recursive
+    
+    git submodule update --init --recursive || {
+        git submodule deinit --all -f
+        rm -rf .git/modules
+        git submodule update --init --recursive 
+    }
     run_in_docker "./build.sh" "${@}"
 }
 
