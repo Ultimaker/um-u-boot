a82cf63e2747046ca7e29fa5b421e59c28a0a7ff
Marijn Deé <m.dee@ultimaker.com>
2021-02-11 14:10:06 +0100

    Changed environment definition, removed emmc and filled in the right addresses [CES-590]

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -48,11 +48,11 @@ env_check()
 run_build()
 {
     
-#    git submodule update --init --recursive || {
-#        git submodule deinit --all -f
-#        rm -rf .git/modules
-#        git submodule update --init --recursive
-#    }
+    git submodule update --init --recursive || {
+        git submodule deinit --all -f
+        rm -rf .git/modules
+        git submodule update --init --recursive
+    }
     run_in_docker "./build.sh" #"${@:-}"
 }
 
